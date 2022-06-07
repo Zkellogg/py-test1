f8bf38058db8a9dcce1020093768102601530cdc
Zkellogg <59624561+Zkellogg@users.noreply.github.com>
2022-06-07 18:12:06 -0400

    Tuesday commit

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,7 +1,19 @@
+import json
 from django.http import JsonResponse
 
 
 def api_home(request, *args, **kwargs):
-    return JsonResponse({"message":"Hi there Django response!" })
+
+    body = request.body
+    data = {}
+    try:
+        data = json.loads(body)
+    except:
+        pass
+    print(data)
+    data['headers'] = dict(request.headers)
+    data['conent_type'] = request.content_type
+
+    return JsonResponse(data)
 
     
