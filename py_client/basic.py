76b1c5f13049c4852da78c84a6d3b8e2aaba65b8
Zkellogg <zackellogg@gmail.com>
2022-06-06 20:10:26 -0400

    first commit

--- py_client/basic.py
+++ py_client/basic.py
@@ -1,11 +1,46 @@
-import pip._vendor.requests 
-
-# endpoint = "https://www.httpbin.org/status/200"
-# endpoint = "https://www.httpbin.org/anything"
-endpoint = "http://127.0.0.1:8000/api/"
+import pip._vendor.requests
 
 
+# endpoint = "https://httpbin.org/status/200"
 
-get_response = pip._vendor.requests.get(endpoint) 
+# endpoint = "https://httpbin.org/anything"
 
-print(get_response.json())
+endpoint = "http://127.0.0.1:8000"
+
+get_response = pip._vendor.requests.get(endpoint, json={"query": "Hello World"})
+print(get_response.status_code)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# import pip._vendor.requests 
+
+# # endpoint = "https://www.httpbin.org/status/200"
+# # endpoint = "https://www.httpbin.org/anything"
+# endpoint = "http://127.0.0.1:8000/api/"
+
+
+
+# get_response = pip._vendor.requests.get(endpoint) 
+
+# print(get_response.json())
